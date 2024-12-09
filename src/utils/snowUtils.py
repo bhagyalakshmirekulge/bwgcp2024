78f6a9211ec89939fbcef54e8496b1c232257793
lakshmi42992 <lakshmirekulge5@gmail.com>
2024-12-09 13:22:50 +0530

    My fourth commit

--- src/utils/snowUtils.py
+++ src/utils/snowUtils.py
@@ -1,5 +1,5 @@
 import snowflake.connector
-from attr import dataclass
+
 
 
 class SnowflakeConnector:
@@ -20,7 +20,8 @@ class SnowflakeConnector:
             account = self.account,
             warehouse = self.warehouse,
             database = self.database,
-            schema = self.schema
+            schema = self.schema,
+            role = self.role
         )
         self.curser = self.connection.cursor()
 
@@ -31,6 +32,30 @@ class SnowflakeConnector:
     def close_connection(self):
         self.curser.close()
         self.connection.close()
-        
+
+user = "GAURI"
+password = "Bhagya@1234"
+account = "ISLXTGM-RM48767"
+warehouse = "COMPUTE_WH"
+database = "SNOWFLAKE_SAMPLE_DATA"
+schema = "TPCH_SF1"
+role = "SYSADMIN"
 
 
+sf_connector = SnowflakeConnector(
+                user = user,
+                password = password,
+                account =  account,
+                warehouse = warehouse,
+                database =  database,
+                schema =  schema,
+                role = role
+                   )
+
+sf_connector.connect()
+
+query = "select * from SNOWFLAKE_SAMPLE_DATA.TPCH_SF1.SUPPLIER limit 10"
+result = sf_connector.execute_query(query)
+print(query)
+
+sf_connector.close_connection()
