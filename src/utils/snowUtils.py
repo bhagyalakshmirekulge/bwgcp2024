596addaefb801e69592f1ddbe1484d5c63bbcbd9
lakshmi42992 <lakshmirekulge5@gmail.com>
2024-12-09 11:57:28 +0530

    My third commit

--- src/utils/snowUtils.py
+++ src/utils/snowUtils.py
@@ -1,1 +1,36 @@
-print("test")
+import snowflake.connector
+from attr import dataclass
+
+
+class SnowflakeConnector:
+    def __init__(self, account, user, password, warehouse, database, schema, role):
+        self.account = account
+        self.user = user
+        self.password = password
+        self.warehouse = warehouse
+        self.database = database
+        self.schema = schema
+        self.role = role
+
+
+    def connect(self):
+        self.connection = snowflake.connector.connect(
+            user = self.user,
+            password = self.password,
+            account = self.account,
+            warehouse = self.warehouse,
+            database = self.database,
+            schema = self.schema
+        )
+        self.curser = self.connection.cursor()
+
+    def execute_query(self, query):
+        self.curser.execute(query)
+        return self.curser.fetchall()
+
+    def close_connection(self):
+        self.curser.close()
+        self.connection.close()
+        
+
+
